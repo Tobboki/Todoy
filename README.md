ff73e99703b6f31856050a80c2a41d1f3c41c155
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2024-04-12 03:33:21 +0200

    HTML: changed the html title, removed unused fonts, improved semanticity, changed task dialogue from dov into dialog element - CSS: removed container class, redesigned the layout , fixed the task dialogue styles - Javascript: changed the task dialogue functionality

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# Welcome 
-## This is a simple todo list web app (still under development).
+# Welcome
 
-## app info:
+## This is a simple todo list web app (still under development)
+
+## App info
+
+### This app is made in plain HTML, CSS, JavaScript
+
 - Design:
-    In this project i used the Neo-Brutalism ui style
-    The layout is inspired by [Nada Ishtewi](https://www.behance.net/nsaeooshy)
+    In this project I used the Neo-Brutalism ui style
+    The layout was inspired by [Nada Ishtewi](https://www.behance.net/nsaeooshy)
     check the design [here!](https://www.behance.net/gallery/107935847/Todo-List-Desktop-Mobile-app-UI-Design?tracking_source=search_projects|todo+list+app&l=7)
-
--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,38 @@
-$(document).ready(function() {
-  const taskDialogue = $('#task-dialogue');
-  const overlay = $('#overlay');
+// task create and edit section
+const taskDialogue = document.getElementById('task-dialogue');
 
-  function openDialogue() {
-    taskDialogue.addClass('active');
-    overlay.addClass('active');
-  }
+function openDialogue() {
+  taskDialogue.showModal();
+}
 
-  function closeDialogue() {
-    taskDialogue.removeClass('active');
-    overlay.removeClass('active');
-  }
+function closeDialogue() {
+  taskDialogue.close();
+}
 
-  $('#create-button').on('click', function() {
-    openDialogue();
-  });
-
-  $('#add-button').on('click', function() {
-    closeDialogue();
-  });
-
-  $('#cancel-button, #overlay').on('click', function() {
-    closeDialogue();
-  });
+//create button functionality
+document.getElementById('create-task-button').addEventListener('click', function() {
+  openDialogue();
 });
+
+//add button functionality
+document.getElementById('add-button').addEventListener('click', function() {
+  closeDialogue();
+});
+
+//cancel button functionality
+document.getElementById('cancel-button').addEventListener('click', function() {
+  closeDialogue();
+});
+
+//close taskDialogue when clicked outside of it (overlay effect)
+taskDialogue.addEventListener("click", e => {
+  const dialogDimensions = taskDialogue.getBoundingClientRect()
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    closeDialogue();
+  }
+})
