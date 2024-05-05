efad70dcafe14f3c69bfabae4db11865a97f1fb7
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2024-05-05 13:53:51 +0300

    Fixed addTask Functionality, getTaskInfo functionality and Added character Limit for the task description input

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,9 @@
-import { renderTasks, toggleTaskOptionsDropdown, openDialogue } from "./taskFrame.js";
+import { renderTasks, openTaskDialogue } from "./taskFrame.js";
 
-//task frame section
-renderTasks();
-
-//create button functionality
+// *create button functionality*
 document.getElementById('create-task-button').addEventListener('click', function() {
-  openDialogue();
+  openTaskDialogue();
 });
 
-const taskOptionsButtons = document.querySelectorAll('.task-options-button');
-taskOptionsButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const dropdownId = button.nextElementSibling.id;
-    toggleTaskOptionsDropdown(dropdownId);
-  });
-});
+// *task frame section*
+renderTasks();
--- scripts/taskFrame.js
+++ scripts/taskFrame.js
@@ -38,7 +38,7 @@ export function renderTasks() {
           <h3 class="task-title">${task.title}</h3>
 
           <div class="task-options-dropdown">
-            <button class="task-options-button" id="task-options-button" type="button">&ctdot;</button> <!-- 3 centered ellipsis unicode icon for editing -->
+            <button class="task-options-button" id="task-options-button" type="button">&ctdot;</button>
 
             <div class="task-options-dropdown-content" id="task-options-dropdown-content-${task.id}">
 
@@ -82,26 +82,109 @@ export function renderTasks() {
 
       taskFrame.innerHTML += taskHTML;
   });
+
+  const taskOptionsButtons = document.querySelectorAll('.task-options-button');
+  taskOptionsButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const dropdownId = button.nextElementSibling.id;
+      toggleTaskOptionsDropdown(dropdownId);
+    });
+  });
+
 };
 
 export function addTask() {
-  const newTaskId = tasks.length + 1; // Generate a unique ID for the new task
-  const { taskTitle, taskDescription } = getTaskInfo();
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const formattedDate = `${day}/${month}/${year}`; //task creation date
+  const taskInfo = getTaskInfo();
 
-  tasks.push({
-      "id": newTaskId.toString(),
-      "title": taskTitle,
-      "description": taskDescription,
-      "creationDate": formattedDate,
-      "isDone": false
-  });
+  if (taskInfo !== null){
+    const { taskTitle, taskDescription } = taskInfo;
+    const newTaskId = (tasks.length + 1).toString();
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const formattedDate = `${day}/${month}/${year}`;
 
-  renderTasks();
+    tasks.push({
+        "id": newTaskId,
+        "title": taskTitle,
+        "description": taskDescription,
+        "creationDate": formattedDate,
+        "isDone": false
+    });
+
+    const newTaskHTML = `
+          <!-- Task Card -->
+          <div class="task-card">
+
+            <!-- Task Card Header-->
+            <div class="task-card-header">
+              <h3 class="task-title">${taskTitle}</h3>
+
+              <div class="task-options-dropdown">
+                <button class="task-options-button" id="task-options-button" type="button">&ctdot;</button>
+
+                <div class="task-options-dropdown-content" id="task-options-dropdown-content-${newTaskId}">
+
+                  <button class="task-edit-button" id="task-edit-button-${newTaskId}" type="button">
+                    <span class="material-symbols-outlined">
+                      edit
+                    </span>
+                    Edit..
+                  </button>
+
+                  <button class="task-remove-button" id="task-remove-button-${newTaskId}" type="button">
+                    <span class="material-symbols-outlined">
+                      delete_forever
+                    </span>
+                    Delete
+                  </button>
+
+                </div>
+              </div>
+            </div>
+
+            <!-- Task Card Body -->
+            <div class="task-card-body">
+              <p class="task-description">${taskDescription}</p>
+            </div>
+
+            <!-- Task Card Footer -->
+            <div class="task-card-footer">
+              <div class="task-checkbox-wrapper">
+                <input class="task-checkbox-input" id="task-checkbox-input-${newTaskId}" type="checkbox" name="task-done" value="done">
+                <label class="task-checkbox-label" for="task-checkbox-input-${newTaskId}">Done</label>
+              </div>
+              <span class="task-create-section">
+                <span class="material-symbols-outlined">calendar_month</span>
+                <time datetime="datetime" class="task-create-date">${formattedDate}</time>
+              </span>
+            </div>
+
+          </div>
+          `;
+
+    const taskFrame = document.querySelector('.task-frame');
+    taskFrame.insertAdjacentHTML('beforeend', newTaskHTML);
+
+    // const taskOptionsButtons = document.querySelectorAll('.task-options-button');
+    // taskOptionsButtons[(newTaskId * 1) - 1].addEventListener('click', () => {
+    //   const dropdownId = button.nextElementSibling.id;
+    //   toggleTaskOptionsDropdown(dropdownId);
+    // });
+
+    const taskOptionsButtons = document.querySelectorAll('.task-options-button');
+    taskOptionsButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const dropdownId = button.nextElementSibling.id;
+        toggleTaskOptionsDropdown(dropdownId);
+      });
+    });
+
+    closeTaskDialogue();
+
+  }
+  
 };
 
 export function removeTask(taskId) {
@@ -117,8 +200,6 @@ export function toggleTaskOptionsDropdown(dropdownId) {
 
   dropdown.classList.toggle('dropdown-content-show');
 };
-
-
 
 // const taskOptionsDropdowns = document.querySelectorAll('.task-options-dropdown-content');
 // taskOptionsDropdowns.forEach(dropdown => {
@@ -139,30 +220,38 @@ export function toggleTaskOptionsDropdown(dropdownId) {
 // *Task dialogue section*
 export const taskDialogue = document.getElementById('task-dialogue');
 
-export function openDialogue() {
+export function openTaskDialogue() {
   taskDialogue.showModal();
 };
 
-export function closeDialogue() {
+export function closeTaskDialogue() {
   taskDialogue.close();
 };
 
 export function getTaskInfo(){
-  const taskTitle = document.getElementById('dialogue-task-title-input').value;
-  const taskDescription = document.getElementById('dialogue-task-description-input').value;
+  const taskTitleInput = document.getElementById('dialogue-task-title-input');
+  const taskDescriptionInput = document.getElementById('dialogue-task-description-input');
+  const taskTitle = taskTitleInput.value;
+  const taskDescription = taskDescriptionInput.value;
 
-  return taskTitle, taskDescription;
+  if (taskTitle !== '' && taskDescription !== '' && taskDescription.length < taskDescriptionCharLimit){
+    taskTitleInput.value = '';
+    taskDescriptionInput.value = '';
+    return {taskTitle, taskDescription};
+  } else{
+    return null;
+  }
+
 } ;
 
 //add button functionality
 document.getElementById('task-dialogue-add-button').addEventListener('click', function() {
-  closeDialogue();
   addTask();
 });
 
 //cancel button functionality
 document.getElementById('task-dialogue-cancel-button').addEventListener('click', function() {
-  closeDialogue();
+  closeTaskDialogue();
 });
 
 //close taskDialogue when clicked outside of it (overlay effect)
@@ -174,6 +263,22 @@ taskDialogue.addEventListener("click", e => {
     e.clientY < dialogDimensions.top ||
     e.clientY > dialogDimensions.bottom
   ) {
-    closeDialogue();
+    closeTaskDialogue();
+  }
+});
+
+// task description input char limit
+const taskDescriptionCharLimit = 255;
+const taskDescriptionCharCounter = document.getElementById('description-input-char-counter');
+taskDescriptionCharCounter.textContent = 0 + "/" + taskDescriptionCharLimit;
+
+const taskDescriptionInput = document.getElementById('dialogue-task-description-input');
+taskDescriptionInput.addEventListener('input', () => {
+  let textLength = taskDescriptionInput.value.length;
+  taskDescriptionCharCounter.textContent = textLength + "/" + taskDescriptionCharLimit;
+
+  if (textLength > taskDescriptionCharLimit) {
+    taskDescriptionInput.style.borderColor = "#e72f41";
+    taskDescriptionCharCounter.style.color = "#e72f41";
   }
 });
