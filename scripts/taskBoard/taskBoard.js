31852326986adc6895856d67342619d5358692a8
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2024-07-22 20:50:38 +0300

    Added the assets folder, changed the css reset to andy bill's basic reset and fixed board.js: 2 dialogue.js linking

--- scripts/taskBoard/taskBoard.js
+++ scripts/taskBoard/taskBoard.js
@@ -1,8 +1,8 @@
 import { renderTasks} from "./taskFrame.js";
-import { openTaskDialogue } from "../taskDialogue.js";
+import { openTaskDialogue } from "./taskDialogue.js";
 
 // *create button functionality*
-document.getElementById('create-task-button').addEventListener('click', function() {
+document.getElementById('create-task-button').addEventListener('click', () => {
   openTaskDialogue('add');
 });
 
